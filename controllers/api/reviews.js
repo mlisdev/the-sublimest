1dd28715f587823d2bfac4000fc6be7cc5565df6
mlisdev <srinehar@syr.edu>
2020-10-13 17:59:21 -0400

    add protected routes

--- controllers/api/reviews.js
+++ controllers/api/reviews.js
@@ -8,22 +8,24 @@ module.exports = {
 };
 
 async function index(req, res) {
-  const reviews = await Review.find({});
-  res.status(200).json(reviews);
+    const reviews = await Review.find({});
+    res.status(200).json(reviews);
 }
 
 async function create(req, res) {
-    // req.body.user = req.user._id; 
-  const review = await Review.create(req.body);
-  res.status(201).json(review);
+    req.body.user = req.user._id; 
+    const review = await Review.create(req.body);
+    res.status(201).json(review);
 }
 
 async function deleteOne(req, res) {
-  const deletedReview = await Review.findByIdAndRemove(req.params.id);
-  res.status(200).json(deletedReview);
+    req.body.user = req.user._id; 
+    const deletedReview = await Review.findByIdAndRemove(req.params.id);
+    res.status(200).json(deletedReview);
 }
 
 async function update(req, res) {
-  const updatedReview = await Review.findByIdAndUpdate(req.params.id, req.body, {new: true});
-  res.status(200).json(updatedReview);
+    req.body.user = req.user._id; 
+    const updatedReview = await Review.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.status(200).json(updatedReview);
 }
