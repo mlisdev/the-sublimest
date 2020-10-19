50ba6439c403deb823aa86fb985d691e92f50bec
mlisdev <srinehar@syr.edu>
2020-10-18 21:31:57 -0400

    index changes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# the-sublimest
+# The Sublimest Trail Finder
+> “He loved the soothing hour, when the last tints of light die away; when the stars, one by one, tremble through æther, and are reflected on the dark mirror of the waters; that hour, which, of all others, inspires the mind with pensive tenderness, and often elevates it to sublime contemplation.” 
+― Ann Radcliffe, The Mysteries of Udolpho
+
+### Screenshots 
+
+### Technologies Used 
+
+### The App 
+
+### Icebox 
--- src/components/SearchArea/SearchArea.jsx
+++ src/components/SearchArea/SearchArea.jsx
@@ -7,14 +7,22 @@ function SearchArea(props, history) {
   return (
       <>
       <Container>
-        <center>
           <div className="hero">
+        <center>
             <h1>hello, {props.user.name}!</h1>
+          </center>
+                  <blockquote className="blockquote mb-0">
+      <p>
+        {' '}
+                “He loved the soothing hour, when the last tints of light die away; when the stars, one by one, tremble through æther, and are reflected on the dark mirror of the waters; that hour, which, of all others, inspires the mind with pensive tenderness, and often elevates it to sublime contemplation.” 
+                ― Ann Radcliffe, The Mysteries of Udolpho
+        erat a ante.{' '}
+            </p>
+            </blockquote>
             </div>
         <SearchBar 
           user={props.user}
           history={history} />
-          </center>
           </Container>
     </>
   );
